b4ab0913e9526108016b6145df9700430d1ffbef
shambumuttalli <shambumuttalli@gmail.com>
2023-03-01 12:47:14 +0530

    comit file

---
+++ printevennumber/printevennumber/printevennumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace printevennumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int a=0;
+           
+            while(a<num)
+            {
+                Console.WriteLine(a);
+                a = a + 2;
+            }
+            Console.ReadLine();
+        }
+    }
+}
